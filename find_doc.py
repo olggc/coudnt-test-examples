ae2bd371f25c519dbf82a686bef31071006e64ba
Olange Guerson <olange.guerson@gmail.com>
2021-01-27 18:35:57 -0300

    readme correctio

--- find_doc.py
+++ find_doc.py
@@ -26,7 +26,7 @@ def main(argv):
         property_value_list = property_value.split(';');
         
         if len(property_name_list) != len(property_value_list):
-            print('If property name is a list so property value must be a list of the same length!')
+            print('If property name is a list so property value must be a list of the same length!\n')
             
             property_name = input("property name (if you provide a list use ';' to separate names): ")
             property_value = input("property value (if you provide a list use ';' to separate values): ")
@@ -36,16 +36,16 @@ def main(argv):
         try:
             client = Cloudant(serviceUsername, servicePassword, url=serviceURL)
             client.connect()
-            print('Client conected to cloudant!')
+            print('Client conected to cloudant!\n')
             if databaseName not in client.all_dbs():
-                print("Database doesnt exist!")
+                print("Database doesnt exist!\n")
             else:
                 db = client[databaseName]
-                print('Client connected to database: %s' % databaseName )          
+                print('Client connected to database: %s\n' % databaseName )          
       
                 selector = dict((property_name_list[i],property_value_list[i]) for i in range(len(property_name_list)))
                 pretty_selector = json.dumps(selector, indent = 4, ensure_ascii=False)
-                print("Searching for %s..." % pretty_selector)
+                print("Searching for... \n%s\n" % pretty_selector)
                 query = Query(db, selector=selector)
                 for doc in query()['docs']:
                     if doc not in doc_list:
@@ -55,11 +55,11 @@ def main(argv):
                     else:
                         continue
                 if len(doc_list) < 1:
-                    print('No documents with specific propriesties found!')
+                    print('No documents with specific propriesties found!\n')
         finally:
             client.disconnect()
     else:
-        print("Too many or too little arguments!")
+        print("Too many or too little arguments!\n")
                
 if __name__ == '__main__':
     main(sys.argv)
